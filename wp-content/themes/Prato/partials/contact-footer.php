d57be9431f2e22011aeb88a2465d7bf4bd98c02b
Yonnys Quintero <leafar.qr@gmail.com>
2020-05-07 17:30:04 -0400

    formularios active campaign

--- wp-content/themes/Prato/partials/contact-footer.php
+++ wp-content/themes/Prato/partials/contact-footer.php
@@ -10,62 +10,164 @@
               <p>quiero recibir información</p>
               <hr>
             </div>
-            <div class="row form-group">
-              <div class="col-md-12 mb-3 mb-md-0">
-                <input type="text" id="fname" class="form-control " placeholder="Nombre " />
+          </style>
+          <form method="POST" action="https://nivel.activehosted.com/proc.php" id="_form_11_" class="_form _form_11 _inline-form  _dark" novalidate>
+            <input type="hidden" name="u" value="11" />
+            <input type="hidden" name="f" value="11" />
+            <input type="hidden" name="s" />
+            <input type="hidden" name="c" value="0" />
+            <input type="hidden" name="m" value="0" />
+            <input type="hidden" name="act" value="sub" />
+            <input type="hidden" name="v" value="2" />
+
+            <!-- utm source -->
+            <input type="hidden" name="field[10]" value="<?php echo $_GET['utm_source']; ?>"/>
+            <!-- utm medium -->
+            <input type="hidden" name="field[11]" value="<?php echo $_GET['utm_medium']; ?>"/>
+            <!-- utm campaign -->
+            <input type="hidden" name="field[12]" value="<?php echo $_GET['utm_campaign']; ?>"/>
+            <!-- utm term -->
+            <input type="hidden" name="field[13]" value="<?php echo $_GET['utm_term']; ?>"/>
+            <!-- referrer url -->
+            <input type="hidden" name="field[14]" value="<?php echo $_SERVER['HTTP_REFERER']; ?>"/>
+
+            <div class="_form-content">
+              <div class="_form_element _x86821064 _full_width " >
+                <label class="_form-label">
+
+                </label>
+                <div class="_field-wrapper">
+                  <input type="text" name="fullname" placeholder="Nombre y Apellido *" />
+                </div>
               </div>
+              <div class="_form_element _x48647240 _full_width " >
+                <label class="_form-label">
 
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12 mb-3 mb-md-0">
-                <input type="text" id="fname" class="form-control " placeholder="Correo" />
+                </label>
+                <div class="_field-wrapper">
+                  <input type="text" name="email" placeholder="Correo electronico*" required/>
+                </div>
               </div>
+              <div class="_form_element _x80842056 _full_width " >
+                <label class="_form-label">
 
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12 mb-3 mb-md-0">
-                <input type="text" id="fname" class="form-control " placeholder="Teléfono" />
+                </label>
+                <div class="_field-wrapper">
+                  <input type="text" name="phone" placeholder="Teléfono *" />
+                </div>
               </div>
+              <div class="_form_element _field1 _full_width " >
+                <label class="_form-label">
 
-            </div>
-
-            <div class="row form-group">
-              <div class="col-md-12 mb-3 mb-md-0">
-                <input type="text" id="fname" class="form-control " placeholder="Ciudad" />
+                </label>
+                <div class="_field-wrapper">
+                  <input type="text" name="field[1]" value="" placeholder="Ciudad" />
+                </div>
               </div>
+              <div class="_form_element _field2 _full_width " >
+                <label class="_form-label">
 
-            </div>
-            <div class="row form-group">
-              <div class="col-md-12 mb-3 mb-md-0">
-                <input type="text" id="fname" class="form-control " placeholder="Mensaje" />
+                </label>
+                <div class="_field-wrapper form-group">
+                  <textarea class="form-control " name="field[2]" placeholder="Mensaje"  ></textarea>
+                </div>
               </div>
+              <div class="_form_element _field4 _full_width " >
+                <div class="_row">
+                  <label class="_form-label">
 
-            </div>
-
-            <div class="col-md-12 center pl-4 mt-2 mb-2">
-              <input type="checkbox" class="custom-control-input " id="customCheck1" name="example1" />
-              <label class="custom-control-label" for="customCheck1">Acepto <a style="color: white !important; border-bottom: 1px solid white; " target="_blank" href="assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf">politicas y
-                privacidad</a></label>
-            </div>
-            <div class="row form-group">
-              <div class="col-md-12 ">
-                <input type="submit" value="Enviar " class="btn btn-black btn-form btn-form--w" />
+                  </label>
+                </div>
+                <input data-autofill="false" type="hidden" name="field[4][]" value="~|">
+                <div class="_row _checkbox-radio">
+                  <input id="field_4Acepto" type="checkbox" name="field[4][]" value="Acepto"   >
+                  <span>
+                    <label for="field_4Acepto">
+                      <span style="color: white; font-size: 14px;" >Acepto</span> <a target="_blank" href="<?php echo get_template_directory_uri();?>/assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf"><span style="color: white; font-size: 14px; border-bottom: 1px solid;" >politicas y
+                      privacidad</span></a>
+                    </label>
+                  </span>
+                </div>
+              </div>
+              <div class="_field-wrapper" style="display: none;">
+                <input type="text" name="field[14]" value="" placeholder="" />
               </div>
             </div>
-
+            <div class="_button-wrapper _full_width">
+              <button id="_form_11_submit" class="_submit btn btn-black btn-form btn-form--w" type="submit">
+                Enviar
+              </button>
+            </div>
+            <div class="_clear-element">
+            </div>
           </div>
-          <div class="col-md-3">
-
+          <div class="_form-thank-you" style="display:none;">
           </div>
+          <div class="_form-branding" style="display: none;">
+            <div class="_marketing-by">
+              Marketing por
+            </div>
+            <a href="http://www.activecampaign.com" class="_logo"></a>
+          </div>
+        </form>
+
+
+        <!-- div class="row form-group">
+          <div class="col-md-12 mb-3 mb-md-0">
+            <input type="text" id="fname" class="form-control " placeholder="Nombre " />
+          </div>
+
         </div>
 
+        <div class="row form-group">
+          <div class="col-md-12 mb-3 mb-md-0">
+            <input type="text" id="fname" class="form-control " placeholder="Correo" />
+          </div>
+
+        </div>
+
+        <div class="row form-group">
+          <div class="col-md-12 mb-3 mb-md-0">
+            <input type="text" id="fname" class="form-control " placeholder="Teléfono" />
+          </div>
+
+        </div>
+
+        <div class="row form-group">
+          <div class="col-md-12 mb-3 mb-md-0">
+            <input type="text" id="fname" class="form-control " placeholder="Ciudad" />
+          </div>
+
+        </div>
+        <div class="row form-group">
+          <div class="col-md-12 mb-3 mb-md-0">
+            <input type="text" id="fname" class="form-control " placeholder="Mensaje" />
+          </div>
+
+        </div>
+
+        <div class="col-md-12 center pl-4 mt-2 mb-2">
+          <input type="checkbox" class="custom-control-input " id="customCheck1" name="example1" />
+          <label class="custom-control-label" for="customCheck1">Acepto <a style="color: white !important; border-bottom: 1px solid white; " target="_blank" href="assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf">politicas y
+          privacidad</a></label>
+        </div>
+        <div class="row form-group">
+          <div class="col-md-12 ">
+            <input type="submit" value="Enviar " class="btn btn-black btn-form btn-form--w" />
+          </div>
+        </div> -->
+
+      </div>
+      <div class="col-md-3">
 
       </div>
     </div>
 
 
-    <span class="number number-2   number-3">06</span>
   </div>
+</div>
+
+
+<span class="number number-2   number-3">06</span>
+</div>
 </section>
